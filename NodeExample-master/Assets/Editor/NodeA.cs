ecff96937ec72363c7156aa160daf8ba75448903
ArcTangent20EggsEggs <svc-student@student.directory.jccc>
2017-05-08 18:22:15 -0500

    NodeThing
    This is really bad, and I assure you I feel bad about it.

---
+++ NodeExample-master/Assets/Editor/NodeA.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeA : NodeBaseClass {
+
+
+    public NodeA (Rect r, int ID) : base(r,ID)
+    {
+        
+    }
+
+    public override void DrawGUI(int winID)
+    {
+        GUILayout.Label(myString);
+        GUILayout.Label(id.ToString());
+        GUILayout.Label("Node : A");
+        foreach(NodeBaseClass n in linkedNodes)
+        {
+            GUILayout.Label("Linked to:  " + n.id);
+        }
+        BaseDraw();
+    }
+
+    public override void AttachComplete(NodeBaseClass winID)
+    {
+        base.linkedNodes.Add(winID);
+    }
+}
